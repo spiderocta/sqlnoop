ebc2ea6d4efa965d8127f777d47b3535136811d5
spiderocta <cryptofer@tuta.io>
2022-05-24 14:05:16 +0200

    added baisc screen

---
+++ octaconnect.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root = Tk()
+root.geometry('1010x550+200+30')
+root.resizable(False,False)
+root.configure(background='white')
+root.title('octaconnect  [Database Manager]')
+root.mainloop()
+
