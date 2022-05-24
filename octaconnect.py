51147e5a3f8ccb2e655e1842238ff28f629e04b4
spiderocta <cryptofer@tuta.io>
2022-05-24 14:14:39 +0200

    added description title

--- octaconnect.py
+++ octaconnect.py
@@ -3,6 +3,13 @@ root = Tk()
 root.geometry('1010x550+200+30')
 root.resizable(False,False)
 root.configure(background='white')
-root.title('octaconnect  [Database Manager]')
+root.title('octaconnect  [Database Manager v1.0(SQL)]')
+root.iconbitmap('images/icon.ico')
+maintTitle = Label(root, text='Database Connector and Manager v1.0 BETA', fg='white', bg='#19282F', font=('tajawal',15))
+maintTitle.pack(fill=X)
+
+
+
+
 root.mainloop()
 
