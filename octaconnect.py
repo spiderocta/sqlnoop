3376eeb038f87dd6a481c771c85fb5ca65909cd0
spiderocta <cryptofer@tuta.io>
2022-05-24 14:41:51 +0200

    added basic table controls buttons

--- octaconnect.py
+++ octaconnect.py
@@ -19,16 +19,37 @@ controlsTitle.pack(fill=X)
 
 # databases controls section
 
-L = Label(F1, text='Show Databases')
+L = Label(F1, text='DB Controls')
 L.place(x=10, y=50)
 
-showAllDataBasesButton = Button(F1, text='All Databases' , cursor='hand2')
+showAllDataBasesButton = Button(F1, text='Show Databases' , cursor='hand2')
 showAllDataBasesButton.place(x=105, y=47, width=125)
 
 hieAllDataBasesButton = Button(F1, text='Hide' , cursor='hand2')
 hieAllDataBasesButton.place(x= 235, y=47, width=55)
 
 
-# databases names section 
+# databases names section
+
+L = Label(F1, text='Database Name : ')
+L.place(x=10, y=80)
+
+En1 = Entry(F1)
+En1.place(x=110, y=80)
+
+createbutton = Button(F1, text='create Db' , cursor='hand2')
+createbutton.place(x=230, y = 78, width=60)
+
+# tables section
+
+L = Label(F1, text='Table Cols: ')
+L.place(x=10, y=110)
+
+createTableButton = Button(F1, text='Creat Table' , cursor='hand2')
+createTableButton.place(x=100, y=107, width=125)
+
+hideTablesButton = Button(F1, text='Hide' , cursor='hand2')
+hideTablesButton.place(x=230, y=107, width=60)
+
 root.mainloop()
 
